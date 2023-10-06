0b4d45389474ebd91d9df9b0e7d54b4ff6be0ef1
Zoltán Novák <novak.zoltan.gy@dolphio.hu>
2023-10-06 12:38:23 +0200

    The start.bat file now creates a VENV for the user

--- start.bat
+++ start.bat
@@ -1,2 +1,24 @@
-call "venv\scripts\activate"
-py main.py
+@echo off
+
+:: Define the name of the virtual environment
+set VENV_NAME=venv
+
+:: Check if the virtual environment already exists, and if not, create it
+if not exist %VENV_NAME% (
+    call python -m venv %VENV_NAME%
+)
+
+:: Activate the virtual environment
+call %VENV_NAME%\Scripts\activate
+
+:: Install dependencies from requirements.txt
+call pip install -r requirements.txt
+
+:: Run the main.py script
+python main.py
+
+:: Deactivate the virtual environment
+deactivate
+
+:: Exit
+exit /b 0
